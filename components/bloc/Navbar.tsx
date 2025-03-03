12a25c76f5266d463ec8cfdadfc68e4da05058c0
Nekena RABEHARIVONJY <safnekena1@gmail.com>
2025-03-03 09:27:38 +0300

    Update README.md

--- components/bloc/Navbar.tsx
+++
@@ -1,7 +0,0 @@
-export const Navbar = () => {
-	return (
-		<nav className='flex justify-between items-center py-4 px-8'>
-			<div className='flex items-center space-x-4 max-w-[1400px] m-auto'></div>
-		</nav>
-	);
-};
